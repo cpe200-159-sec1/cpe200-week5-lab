0a3591bd1d808fbcf9784d96a568ace25ddd44ee
smile1218 <supitcha.sawasd@hotmail.com>
2016-09-20 17:13:46 +0700

    580610684

--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,26 +4,46 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
 
     public User() {
+        setUserName("");
+        setPassword("");
     }
 
-    public String setUserName(String name) {
-        return null;
+    public String setUserName(String name)  {
+        String A = "[a-zA-Z][A-Za-z0-9]{7,}";
+        String oldname = null;
+        if ( name != null && !name.equals("")) {
+            if (name.matches(A)) {
+                oldname = userName;
+                userName = name;
+            } else {
+                throw new RuntimeException("false");
+            }
+        }
+        return oldname;
     }
 
     public int setPassword(String name) {
-        return 0;
-    }
+        String B = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{12,}";
+        if ( name != null && !name.equals("")) {
+            if (name.matches(B)) {
+                password = name;
+            } else
+                throw new RuntimeException("false");
+        }
+        return name.length();
+        }
+
+
 
     public String getUserName() {
-
-        return null;
+        return this.userName;
     }
 
     public String getPassword() {
-        return null;
+        return this.password;
     }
 }
