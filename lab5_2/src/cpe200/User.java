34c9e89570dca7d6820df62b7edf319b79fb0217
kohSatrarin <koh.satrarin@gmail.com>
2016-09-20 21:27:20 +0700

    580610679

--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,26 +4,42 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
+    private String userPattern = "^[A-Za-z][A-Za-z0-9]{7,}$";
+    private String passPattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{12,}$";
 
     public User() {
+        userName = null;
+        password = null;
     }
 
     public String setUserName(String name) {
-        return null;
+        if (!name.matches(userPattern) || name.equals("") || name.equals("null")){
+            //userName = "null";
+            throw new RuntimeException("Wrong input.");
+        }
+            String oldUserName = userName;
+            this.userName = name;
+            return oldUserName;
+
     }
 
-    public int setPassword(String name) {
-        return 0;
+    public int setPassword(String pass) {
+        if (!pass.matches(passPattern) || pass.equals("") || pass.equals("null")){
+            throw new RuntimeException("Wrong input.");
+        }
+        password = pass;
+        return password.length();
     }
 
     public String getUserName() {
 
-        return null;
+
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
