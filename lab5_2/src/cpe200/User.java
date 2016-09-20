fa2a10263153b68007190499b69c2de933118f50
Uttakran Renuman <qowieury_kidmaioak@hotmail.com>
2016-09-20 16:12:04 +0700

    lab 5-2 complete

--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -7,23 +7,77 @@ public class User {
     public String userName;
     public String password;
 
-    public User() {
+    public User(){
+
+    }
+    public User(String user ,String pass) {
+        userName = user;
+        password = pass;
     }
 
     public String setUserName(String name) {
-        return null;
+        String tmp = userName;
+        if(isUsernameValid(name)){
+            userName = name;
+        }else {
+            throw new RuntimeException();
+        }
+        return tmp;
     }
 
     public int setPassword(String name) {
-        return 0;
+        if(isPasswordValid(name)){
+            password = name;
+        }else{
+            throw new RuntimeException();
+        }
+        return password.length();
     }
 
     public String getUserName() {
 
-        return null;
+        return userName;
     }
 
-    public String getPassword() {
-        return null;
+    public String getPassword(){
+        return password;
     }
+
+
+    public boolean isUsernameValid(String name) {
+        if (name.length() < 8){//check lengh >=8
+            return false;
+        }else if (Character.isDigit(name.charAt(0)) == true) {
+            return false;
+        }else {
+            for (int i = 0; i < name.length(); i++) {
+                if (Character.isAlphabetic(name.charAt(i)) == false) {
+                    if (Character.isDigit(name.charAt(i)) == false){
+                        return false;
+                    }
+                }
+            }
+            return true;
+
+
+        }
+    }
+    public boolean isPasswordValid(String name){
+        if (name.length() >=12){
+            boolean haveDigit=false,haveCapital=false,haveSmall=false;
+            for (int i=0;i<name.length();i++)
+            {
+                if(Character.isUpperCase(name.charAt(i))) haveCapital=true;
+                else if(Character.isLowerCase(name.charAt(i))) haveSmall=true;
+                else if(Character.isDigit(name.charAt(i))) haveDigit=true;
+                if(haveCapital && haveDigit && haveSmall){
+                    password = name;
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+
 }
