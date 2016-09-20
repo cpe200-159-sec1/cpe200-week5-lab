8e40f451c53ad44dff060fdfd920aaf91789e934
ood9842 <ood9842@hotmail.com>
2016-09-20 17:14:35 +0700

    580610629

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,98 @@
 package cpe200;
 
+import java.math.BigDecimal;
 
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand=operand;
     }
 
-
     public void setSecondOperand(Operand operand) {
+        secondOperand=operand;
     }
 
     public String add() throws RuntimeException {
-        return null;
+        BigDecimal num1=new BigDecimal(firstOperand.getOperand());
+        BigDecimal num2=new BigDecimal(secondOperand.getOperand());
+        if(num1.intValue()<0||num2.intValue()<0)
+        {
+            throw new RuntimeException("ERROR");
+        }
+        else
+        {
+            BigDecimal sent=num1.add(num2);
+            return sent.stripTrailingZeros().toString();
+        }
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        BigDecimal num1=new BigDecimal(firstOperand.getOperand());
+        BigDecimal num2=new BigDecimal(secondOperand.getOperand());
+        if(num1.intValue()<0||num2.intValue()<0)
+        {
+            throw new RuntimeException("ERROR");
+        }
+        else
+        {
+            BigDecimal sent=num1.subtract(num2);
+            return sent.stripTrailingZeros().toString();
+        }
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        BigDecimal num1=new BigDecimal(firstOperand.getOperand());
+        BigDecimal num2=new BigDecimal(secondOperand.getOperand());
+        if(num1.intValue()<0||num2.intValue()<0)
+        {
+            throw new RuntimeException("ERROR");
+        }
+        else
+        {
+            BigDecimal sent=num1.multiply(num2);
+            return sent.stripTrailingZeros().toString();
+        }
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        BigDecimal num1=new BigDecimal(firstOperand.getOperand());
+        BigDecimal num2=new BigDecimal(secondOperand.getOperand());
+        if(num1.intValue()<0||num2.intValue()<0)
+        {
+            throw new RuntimeException("ERROR");
+        }
+        else if(num2.intValue()==0)
+        {
+            throw new ArithmeticException("ERROR divisor is zero");
+        }
+        else
+        {
+            BigDecimal sent=num1.divide(num2,5,5);
+            return sent.stripTrailingZeros().toString();
+        }
     }
 
     public String power() throws RuntimeException {
-        return null;
+        BigDecimal num1=new BigDecimal(firstOperand.getOperand());
+        BigDecimal num2=new BigDecimal(secondOperand.getOperand());
+        if(num1.intValue()<0||num2.intValue()<0)
+        {
+            throw new RuntimeException("ERROR");
+        }
+        else
+        {
+            double dnum1=num1.doubleValue();
+            double dnum2=num2.doubleValue();
+            Operand temp=new Operand(Math.pow(dnum1,dnum2));
+            BigDecimal sent=new BigDecimal(temp.getOperand());
+            return sent.stripTrailingZeros().toString();
+        }
     }
 
 }
--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -7,44 +7,79 @@ import java.util.ArrayList;
  * Created by pruet on 12/9/2559.
  */
 public class Users {
-    public ArrayList<User> userList;
+    private ArrayList<User> userList;
 
+    public Users()
+    {
+        userList=new ArrayList<>();
+    }
     public void addUser(User user)
     {
+        userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        User temp=new User(userName,password);
+        userList.add(temp);
     }
 
-    public void deleteUser(User user)
-    {
-
+    public void deleteUser(User user)throws RuntimeException {
+        if(!exists(user)||userList.size()==0)
+        {
+            throw new RuntimeException();
+        }
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i) == user) {
+                userList.remove(i);
+            }
+        }
     }
 
     public boolean exists(User user)
     {
+        for(int i=0;i<userList.size();i++)
+        {
+            if(userList.get(i)==user)
+            {
+                return true;
+            }
+        }
         return false;
     }
 
     public boolean usernameExists(String username)
     {
+        for(int i=0;i<userList.size();i++)
+        {
+            if(userList.get(i).getUserName()==username)
+            {
+                return true;
+            }
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
     public User getUserByUsername(String userName)
     {
+        for(int i=0;i<userList.size();i++)
+        {
+            if(userList.get(i).getUserName()==userName)
+            {
+                return userList.get(i);
+            }
+        }
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
     public User[] getUserArray()
     {
-        return null;
+        return userList.toArray(new User[0]);
     }
 }
