71639d2027fbb2bc489e00a69b1d8c62216cf20f
ather <kandanich@hotmail.com>
2016-09-20 15:28:45 +0700

    Update bug fix

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,22 +1,33 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
+
+    private BigDecimal x;
+    private BigDecimal y;
 
     public BinaryCalculator() {
+        x = new BigDecimal(0);
+        y = new BigDecimal(0);
     }
 
     public void setFirstOperand(Operand operand) {
+        x = new BigDecimal(operand.getOperand());
     }
 
 
     public void setSecondOperand(Operand operand) {
+        y = new BigDecimal(operand.getOperand());
     }
 
-    public String add() throws RuntimeException {
-        return null;
+    public String add() {
+        if (x.intValue() < 0 || y.intValue() < 0){
+            throw new RuntimeException("Operand must not negative");
+        }return x.add(y).stripTrailingZeros().toString();
     }
 
     public String subtract() throws RuntimeException {
