378f8fd5c45b8cf659ac5e01705f629a79251aba
xXxRosxXx <suttaweelukuan@gmail.com>
2016-09-20 17:30:34 +0700

    580610683

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,66 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
+    private BigDecimal FOP;
+    private BigDecimal SOP;
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand = operand;
+        FOP = new BigDecimal(firstOperand.getOperand());
     }
 
 
     public void setSecondOperand(Operand operand) {
+        secondOperand = operand;
+        SOP = new BigDecimal(secondOperand.getOperand());
     }
 
     public String add() throws RuntimeException {
-        return null;
-    }
 
+
+        if(FOP.intValue() < 0 || SOP.intValue() < 0){
+            throw new RuntimeException("Operand is negative");
+        }
+        return FOP.add(SOP).stripTrailingZeros().toString();
+    }
     public String subtract() throws RuntimeException {
-        return null;
+        // return null;
+        if(FOP.intValue() < 0 || SOP.intValue() < 0){
+            throw new RuntimeException("Operand is negative");
+        }
+        return FOP.subtract(SOP).stripTrailingZeros().toString();
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        if(FOP.intValue() < 0 || SOP.intValue() < 0){
+            throw new RuntimeException("Operand is negative");
+        }
+        return FOP.multiply(SOP).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        if(SOP.equals(0)) {
+            throw new ArithmeticException("ERROR");
+        }
+        else if(FOP.intValue() < 0 || SOP.intValue() < 0){
+            throw new RuntimeException("Operand is negative");
+        }
+        return FOP.divide(SOP,5,BigDecimal.ROUND_UP).stripTrailingZeros().toString();
     }
 
     public String power() throws RuntimeException {
-        return null;
+        if(FOP.intValue() < 0 || SOP.intValue() < 0){
+            throw new RuntimeException("Operand is negative");
+        }
+        return FOP.pow(SOP.intValue()).stripTrailingZeros().toString();
     }
-
 }
--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,26 +4,50 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName=null;
+    private String password;
 
     public User() {
     }
-
-    public String setUserName(String name) {
-        return null;
+    public User(String userName,String password)
+    {
+        this.setUserName(userName);
+        this.setPassword(password);
+    }
+    public String setUserName(String name)
+    {
+        if (name.matches("^[a-zA-Z][a-zA-Z0-9]{7,}$"))
+        {
+            String temp=userName;
+            this.userName = name;
+            return temp;
+        }
+        else
+        {
+            throw new RuntimeException("ERROR Invalid username");
+        }
     }
 
-    public int setPassword(String name) {
-        return 0;
+    public int setPassword(String name)
+    {
+        if(name.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{12,}$"))
+        {
+            this.password=name;
+            return password.length();
+        }
+        else
+        {
+            throw new RuntimeException("ERROR Invalid password");
+        }
     }
 
     public String getUserName() {
 
-        return null;
+        return this.userName;
     }
 
-    public String getPassword() {
-        return null;
+    public String getPassword()
+    {
+        return this.password;
     }
 }
