f8a3bc64ce942b01cefc4b72f81a1e61babefe12
omhsin007 <makoto.diva@gmail.com>
2016-09-20 16:08:16 +0700

    580610648

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,72 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand ;
+    private Operand secondOperand;
+
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand = operand;
     }
 
 
     public void setSecondOperand(Operand operand) {
+        secondOperand = operand;
     }
 
-    public String add() throws RuntimeException {
-        return null;
+    public String add() {
+        BigDecimal temp1 = new BigDecimal(firstOperand.Getoper());
+        BigDecimal temp2 = new BigDecimal(secondOperand.Getoper());
+        if (temp1.compareTo(BigDecimal.ZERO) >= 0 && temp2.compareTo(BigDecimal.ZERO) >= 0)
+        return temp1.add(temp2).stripTrailingZeros().toString();
+        else
+        throw new RuntimeException();
     }
 
-    public String subtract() throws RuntimeException {
-        return null;
+    public String subtract() {
+        BigDecimal temp1 = new BigDecimal(firstOperand.Getoper());
+        BigDecimal temp2 = new BigDecimal(secondOperand.Getoper());
+        if (temp1.compareTo(BigDecimal.ZERO) >= 0 && temp2.compareTo(BigDecimal.ZERO) >= 0)
+        return temp1.subtract(temp2).stripTrailingZeros().toString();
+        else
+            throw new RuntimeException();
     }
 
-    public String multiply() throws RuntimeException {
-        return null;
+    public String multiply() {
+        BigDecimal temp1 = new BigDecimal(firstOperand.Getoper());
+        BigDecimal temp2 = new BigDecimal(secondOperand.Getoper());
+        if (temp1.compareTo(BigDecimal.ZERO) >= 0 && temp2.compareTo(BigDecimal.ZERO) >= 0)
+        return temp1.multiply(temp2).stripTrailingZeros().toString();
+        else
+            throw new RuntimeException();
     }
 
     /* This method should throw an exception when divide by zero */
-    public String division() throws RuntimeException {
-        return null;
+    public String division() throws ArithmeticException {
+        BigDecimal temp1 = new BigDecimal(firstOperand.Getoper());
+        BigDecimal temp2 = new BigDecimal(secondOperand.Getoper());
+        if (temp1.compareTo(BigDecimal.ZERO) >= 0 && temp2.compareTo(BigDecimal.ZERO) >= 0)
+        return temp1.divide(temp2,5, RoundingMode.HALF_UP).stripTrailingZeros().toString();
+        else {
+            if (temp2.compareTo(BigDecimal.ZERO) == 0)
+                throw new ArithmeticException();
+            throw new RuntimeException();
+        }
     }
 
-    public String power() throws RuntimeException {
-        return null;
+    public String power() {
+        BigDecimal temp1 = new BigDecimal(firstOperand.Getoper());
+        BigDecimal temp2 = new BigDecimal(secondOperand.Getoper());
+        if (temp1.compareTo(BigDecimal.ZERO) >= 0 && temp2.compareTo(BigDecimal.ZERO) >= 0)
+        return temp1.pow(temp2.intValue()).stripTrailingZeros().toString();
+        else
+            throw new RuntimeException();
     }
-
 }
--- lab5_1/src/cpe200/Operand.java
+++ lab5_1/src/cpe200/Operand.java
@@ -4,11 +4,21 @@ public class Operand {
     private String operand;
 
     public Operand(String operand) {
+        this.operand = operand;
     }
 
     public Operand(int operand) {
+        this.operand = Integer.toString(operand);
     }
 
     public Operand(double operand) {
+        this.operand = Double.toString(operand);
     }
+
+    public String Getoper() {
+        return operand;
+    }
+
+
+
 }
