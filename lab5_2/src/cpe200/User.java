eabd90091e236513dbe61ac8505103367e4869a2
chalitphol kuensuwan <chalitphol@gmail.com>
2016-09-20 17:40:04 +0700

    LAB 5 580610630

--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,26 +4,42 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
 
     public User() {
     }
 
-    public String setUserName(String name) {
-        return null;
+    public String setUserName(String name) throws RuntimeException
+    {
+        String uName = userName;
+            String checkName = "^[a-zA-Z][a-zA-Z0-9]{7,}$";
+            if(name.matches(checkName)){
+                userName = name;
+            }
+            else
+                throw new RuntimeException("Error");
+        return uName;
     }
 
-    public int setPassword(String name) {
-        return 0;
+    public int setPassword(String name)
+    {   String checkPassword = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{12,}$";
+        if(name.matches(checkPassword)){
+            password = name;
+        }
+        else {
+            throw new RuntimeException("Error");
+        }
+        return name.length();
     }
 
     public String getUserName() {
 
-        return null;
+        return userName;
     }
 
-    public String getPassword() {
-        return null;
+    public String getPassword()
+    {
+        return password;
     }
 }
