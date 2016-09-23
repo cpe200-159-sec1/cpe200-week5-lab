9526e637494ca3db4744f0effafa71436440eed0
patdanai665 <patdanai.duang@gmail.com>
2016-09-23 17:52:25 +0700

    finish

--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -3,27 +3,45 @@ package cpe200;
 /**
  * Created by pruet on 6/9/2559.
  */
+
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
 
     public User() {
     }
 
-    public String setUserName(String name) {
-        return null;
+    public  User(String userName, String password){
+        setPassword(password);
+        setUserName(userName);
     }
 
-    public int setPassword(String name) {
-        return 0;
+    public String setUserName(String name) throws RuntimeException {
+
+        String valid = "^[a-zA-Z][a-zA-Z0-9]{7,}$";
+        String oldName = userName;
+        userName = name;
+        if (name.matches(valid) && name!= "") {
+            userName = name;
+            return oldName;
+        }
+        throw new RuntimeException("Invalid name");
+    }
+
+    public int setPassword(String name) throws RuntimeException {
+        String valid = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{9,}$";
+        if (name.matches(valid)&& name!= "") {
+            password = name;
+            return password.length();
+        }
+        throw new RuntimeException("Invalid password");
     }
 
     public String getUserName() {
-
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
