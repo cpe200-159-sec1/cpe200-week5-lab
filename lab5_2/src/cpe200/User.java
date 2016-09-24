88c3db57b23fb1117d4f2381ae421411489bf634
Potsawat <potsawat_3232@hotmail.com>
2016-09-24 17:19:44 +0700

    lab 5-2 Done

--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -8,22 +8,34 @@ public class User {
     public String password;
 
     public User() {
+        userName = null;
+        password = null;
     }
 
     public String setUserName(String name) {
-        return null;
+        String x = "[A-Za-z][A-Za-z0-9]{7,}";
+        if (name.matches(x) && !(name.equals(null))) {
+            String tmp = userName;
+            userName = name;
+            return tmp;
+        }
+        throw new RuntimeException("Error");
     }
 
     public int setPassword(String name) {
-        return 0;
+        String x = "(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{12,}";
+        if (name.matches(x) && !(name.equals(null))) {
+            password = name;
+            return password.length();
+        }
+        throw new RuntimeException("Error");
     }
 
     public String getUserName() {
-
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -7,44 +7,61 @@ import java.util.ArrayList;
  * Created by pruet on 12/9/2559.
  */
 public class Users {
-    public ArrayList<User> userList;
+    public ArrayList<User> userList = new ArrayList<>();
+
 
     public void addUser(User user)
     {
+        userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        User node = new User();
+        node.setUserName(userName);
+        node.setPassword(password);
+
+        userList.add(node);
     }
 
     public void deleteUser(User user)
     {
-
+        if (exists(user))
+            userList.remove(user);
+        else throw new RuntimeException("Error");
     }
 
     public boolean exists(User user)
     {
-        return false;
+        return userList.contains(user);
     }
 
     public boolean usernameExists(String username)
     {
+        for (User i : userList) {
+            if (i.getUserName().equals(username))
+                return true;
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
     public User getUserByUsername(String userName)
     {
+        for (User i : userList) {
+            if (i.getUserName().equals(userName))
+                return i;
+        }
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
     public User[] getUserArray()
     {
-        return null;
+        return userList.toArray(new User[1]);
     }
 }
