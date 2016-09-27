146f938389cea858514b282ba334ae621a53875c
drum2007 <beam555@hotmail.com>
2016-09-27 10:35:07 +0700

    Lab5_1 done Lab5_2

--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -7,28 +7,40 @@ import java.util.ArrayList;
  * Created by pruet on 12/9/2559.
  */
 public class Users {
-    public ArrayList<User> userList;
+    public ArrayList<User> userList = new ArrayList<>();
 
     public void addUser(User user)
     {
+        userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        User a = new User();
+        a.setUserName(userName);
+        a.setPassword(password);
     }
 
     public void deleteUser(User user)
     {
-
+        if (exists(user)){
+            userList.remove(user);
+        }else {
+            throw new RuntimeException();
+        }
     }
 
     public boolean exists(User user)
     {
+        if (userList.contains(user)) return true;
         return false;
     }
 
     public boolean usernameExists(String username)
     {
+        for (User i : userList){
+            if (i.userName.equals(username)) return true;
+        }
         return false;
     }
 
